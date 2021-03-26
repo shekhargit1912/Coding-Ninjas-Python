dfb7905f4686c89b6be8698523cf058cc19f7988
Shekhar Chaugule <79699567+shekhargit1912@users.noreply.github.com>
2021-03-26 12:42:46 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,23 @@
+def checkPalindrome(num):
+    temp=num
+    rev=0
+    while num>0:
+        rem=num%10
+        
+        rev=(rev*10)+rem
+        num=num//10
+    if(temp==rev):
+        return True
+    else:
+        return False
+        
+        
+    # rev=num
+#
+		
+num = int(input())
+isPalindrome = checkPalindrome(num)
+if(isPalindrome):
+	print('true')
+else:
+	print('false')
