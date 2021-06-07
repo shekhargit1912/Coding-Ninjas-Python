628f710267c96e7fd549e5868a277941019ebab8
Shekhar Chaugule <79699567+shekhargit1912@users.noreply.github.com>
2021-06-07 19:24:30 +0530

    Create insertionsort.py

---
+++ SEarchingandSorting/insertionsort.py
@@ -0,0 +1,65 @@
+from sys import stdin
+
+def insertionSort(arr, n) :  
+    
+    for i in range(n):
+        j=i-1
+        temp=arr[i]
+        while j>=0 and arr[j]>temp:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=temp
+            
+            
+    return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().strip())
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+#to print the array/list
+def printList(arr, n) : 
+    for i in range(n) :
+        print(arr[i], end = " ")
+        
+    print()
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    insertionSort(arr, n)
+    printList(arr, n)
+
+    t-= 1
