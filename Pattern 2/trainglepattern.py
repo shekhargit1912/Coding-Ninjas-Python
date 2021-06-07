2dd147c65c7f5f5619673b4347b3db80302f4f32
Shekhar Chaugule <79699567+shekhargit1912@users.noreply.github.com>
2021-06-07 19:09:03 +0530

    Create trainglepattern.py

---
+++ Pattern 2/trainglepattern.py
@@ -0,0 +1,21 @@
+n=int(input())
+i=1
+while i<=n:
+    s=1
+    while s<=(n-i):
+        print(" ",end="")
+        s+=1
+    j=1
+    val=i
+    while j<=i:
+        print(val,end="")
+        val+=1
+        j+=1
+    k=1
+    val=2*i-2
+    while k<=i-1:
+        print(val,end="")
+        val-=1
+        k+=1
+    print()
+    i+=1
